67c8ef605574e668a1ac586ee71284a3b5c992f3
julian <github2018@studio42.dk>
2018-08-09 13:33:19 +0200

    build

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dappform_forms_api_1 = require("dappform-forms-api");
 const express = require("express");
+const write_1 = require("dappform-forms-api/dist/lib/write");
+const requestPromise = require("request-promise-native");
 const wt = require('webtask-tools');
 const loadBlockstack = require('blockstack-anywhere');
 const blockstack = require('blockstack');
@@ -15,7 +17,12 @@ function initBlockstack(context) {
     loadBlockstack();
 }
 async function handleSubmission(publicKey, encryptedData) {
-    return await dappform_forms_api_1.newFormSubmission(JSON.parse(blockstack.decryptContent(encryptedData, { privateKey })));
+    const submission = JSON.parse(blockstack.decryptContent(encryptedData, { privateKey }));
+    await dappform_forms_api_1.newFormSubmission(submission);
+    const settings = await write_1.getFile('settings.json');
+    if (settings && settings.webhookUrl) {
+        await simpleWebhook(settings.webhookUrl, submission);
+    }
 }
 const app = express();
 app.use(cors());
@@ -26,13 +33,27 @@ app.post('/', (req, res) => {
         initBlockstack(req.webtaskContext);
         const key = req.body.key;
         const encryptedDataString = JSON.stringify(req.body.data);
-        handleSubmission(key, encryptedDataString).then((d) => {
-            return res.send(d);
-        });
+        handleSubmission(key, encryptedDataString)
+            .then(() => res.end());
     }
     else {
         res.status(500).send('no data submitted');
     }
 });
+async function simpleWebhook(url, submission) {
+    try {
+        const res = await requestPromise.post(url, {
+            json: submission
+        });
+        console.log("Did sent webhook. Status: ", res.statusCode);
+    }
+    catch (e) {
+        console.error("Failed sending webhook:");
+        console.error(e.message);
+    }
+}
 module.exports = wt.fromExpress(app);
+app.listen(3000, () => {
+    simpleWebhook("http://localhost:3000", { "data": {} });
+});
 //# sourceMappingURL=index.js.map
